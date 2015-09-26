11f3c7af6bf6939a4d7a3e5c654282c169efd943
Joannah Nanjekye <nanjekyejoannah@gmail.com>
2015-09-26 14:01:10 +0300

    removed tests

--- todo/tests.py
+++ todo/tests.py
@@ -1,12 +1,12 @@
 
-from django.test import TestCase
+# from django.test import TestCase
 
 
-class CodefishTest(TestCase):
-    def test_entered_the_home_page(self):
-        response = self.client.get('/')
-        self.assertContains(response, "yaya ")
+# class ToDoTest(TestCase):
+#     def test_entered_the_home_page(self):
+#         response = self.client.get('/')
+#         self.assertContains(response, "yaya ")
 
-    def test_leads_to_the_create_page(self):
-        response = self.client.get('/')
-        self.assertContains(response, '<a href="/create">' )
+#     def test_leads_to_the_create_page(self):
+#         response = self.client.get('/')
+#         self.assertContains(response, '<a href="/create">' )
